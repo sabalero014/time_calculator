a7ae19d8bb03f45920a9aea2aa3a767642788159
sabalero014 <sabalero014@users.noreply.github.com>
2020-12-01 21:04:37 -0300

    suma de horas
    voy sumando las horas y minutos

--- pruebas_sueltas.py
+++ pruebas_sueltas.py
@@ -1,5 +1,31 @@
 start = "12:35 AM"
+duration = "3:15"
 new_start = start.replace(" ",":").split(":")
+#separo también DURATION
+new_duration = duration.split(":")
 
-print(new_start)
-print(new_start[1])
+#suma de minutos
+suma_horas = 0 #ya dejo planteada la duración en horas para poder sumarla en el condicional
+suma_minutos = int(new_start[1]) + int(new_duration[1])
+
+if suma_minutos > 60:
+    suma_horas += 1
+    suma_minutos = suma_minutos - 60
+elif 0 < suma_minutos < 60:
+    pass
+else:
+    print("algo raro pasa con los minutos")
+
+#diferencio entre AM y PM
+if new_start [2] =="PM":
+    suma_horas = suma_horas + 12
+else:
+    pass
+
+#sumo las horas
+suma_horas = suma_horas + int(new_start[0]) + int(new_duration[0])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+
+
+#impresiones de prueba
+print(suma_minutos)
+print(suma_horas)
--- time_calculator.py
+++ time_calculator.py
@@ -7,4 +7,25 @@ def add_time(start, duration):
     #separo también DURATION
     new_duration = duration.split(":")
 
+    #suma de minutos
+    duracion_horas = 0 #ya dejo planteada la duración en horas para poder sumarla en el condicional
+    duracion_minutos = new_start[1] + new_duration[1]
+    if duracion_minutos > 60:
+        duracion_horas += 1
+        duracion_minutos = duracion_minutos - 60
+    elif 0 < duracion_minutos < 60:
+        pass
+    else:
+        print("algo raro pasa con los minutos")
+    
+    #diferencio entre AM y PM
+    if new_start =="PM":
+        duracion_horas = duracion_horas + 12
+    else:
+        pass
+    
+    #sumo las horas
+    duracion_horas = duracion_horas + new_start[0] + new_duration[0]
+    
+
     return new_time
