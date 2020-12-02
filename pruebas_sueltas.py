58c7ac092304ee9e2fc9e0042c9553df6bebe1ec
sabalero014 <sabalero014@users.noreply.github.com>
2020-12-01 21:19:43 -0300

    lista de valores sumados

--- pruebas_sueltas.py
+++ pruebas_sueltas.py
@@ -4,6 +4,10 @@ new_start = start.replace(" ",":").split(":")
 #separo también DURATION
 new_duration = duration.split(":")
 
+#lista de valores sumados
+#formato final: [hora, minutos, AM/PM, días]
+sumados = []
+
 #suma de minutos
 suma_horas = 0 #ya dejo planteada la duración en horas para poder sumarla en el condicional
 suma_minutos = int(new_start[1]) + int(new_duration[1])
@@ -25,6 +29,9 @@ else:
 #sumo las horas
 suma_horas = suma_horas + int(new_start[0]) + int(new_duration[0])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
+#lista de los valores sumados nuevamente. Va al final de todo, con las variables copiadas
+sumados = [suma_horas,suma_minutos]
+#faltan 2 valores todavia
 
 #impresiones de prueba
 print(suma_minutos)
